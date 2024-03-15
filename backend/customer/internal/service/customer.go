8b4807ddbbfaac7af86570634e1748083c3a3528
rotter <1559128037@qq.com>
2024-03-15 18:19:40 +0800

    实现顾客服务的获取验证码接口及业务逻辑，添加了docker-compose.yml

---
+++ backend/customer/internal/service/customer.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"context"
+	"regexp"
+	"time"
+
+	pb "customer/api/customer"
+	"customer/api/verifyCode"
+
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+)
+
+type CustomerService struct {
+	pb.UnimplementedCustomerServer
+}
+
+func NewCustomerService() *CustomerService {
+	return &CustomerService{}
+}
+
+func (s *CustomerService) GetVerifyCode(ctx context.Context, req *pb.GetVerifyCodeReq) (*pb.GetVerifyCodeRes, error) {
+	//一、校验手机号
+	pattern := `^(13\d|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18\d|19[0-35-9])\d{8}$`
+	regexpPattern := regexp.MustCompile(pattern)   //编译正则表达式字符串
+	if !regexpPattern.MatchString(req.Telephone) { //执行匹配操作
+		return &pb.GetVerifyCodeRes{
+			Code:    1,
+			Message: "电话号码格式错误",
+		}, nil
+	}
+
+	//二、通过验证码生成服务获取验证码（服务间通信，grpc）
+	// 连接目标grpc服务器
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint("localhost:9000"), //verifyCode grpc service 地址
+	)
+	if err != nil {
+		return &pb.GetVerifyCodeRes{
+			Code:    1,
+			Message: "验证码服务不可用",
+		}, nil
+	}
+	//关闭
+	defer func() {
+		_ = conn.Close()
+	}()
+	//发送验证码请求
+	client := verifyCode.NewVerifyCodeClient(conn)
+	response, err := client.GetVerifyCode(context.Background(), &verifyCode.GetVerifyCodeRequest{
+		Length: 6,
+		Type:   1,
+	})
+	if err != nil {
+		return &pb.GetVerifyCodeRes{
+			Code:    1,
+			Message: "验证码获取失败",
+		}, nil
+	}
+
+	return &pb.GetVerifyCodeRes{
+		Code:           0,
+		VerifyCode:     response.Code,
+		VerifyCodeTime: time.Now().Unix(),
+		VerifyCodeLife: 60,
+	}, nil
+}
